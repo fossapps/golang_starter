9f4cfb2e80e192706bf93b1159002d312bc508e0
Nishchal Gautam <gautam.nishchal@gmail.com>
2018-03-24 07:46:16 +0700

    chore(typo): fix typo

--- helpers/cache.go
+++ helpers/cache.go
@@ -2,7 +2,7 @@ package helpers
 
 import (
 	"time"
-	//"crazy_nl_backend"
+	// "crazy_nl_backend"
 )
 
 func Remember(key string, handler func() string, duration time.Duration) string {
@@ -10,8 +10,8 @@ func Remember(key string, handler func() string, duration time.Duration) string 
 	// check if it's expired
 	// if everything is ok, return
 	// if expired/not exists, call handler func
-	//redis := crazy_nl_backend.GetRedis()
-	//result, _ := redis.Get("cache" + key).Result()
-	//redis.Set("cache|" + key, handler(), duration).Result()
+	// redis := crazy_nl_backend.GetRedis()
+	// result, _ := redis.Get("cache" + key).Result()
+	// redis.Set("cache|" + key, handler(), duration).Result()
 	return ""
 }
