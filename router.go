b287ab0d76705cd1bde6a68a9d94ce2747599a18
Nishchal Gautam <gautam.nishchal@gmail.com>
2018-04-26 05:52:36 +0700

    refactor(router): separate resources into multiple functions

--- router.go
+++ router.go
@@ -5,36 +5,61 @@ import (
 
 	"github.com/gorilla/mux"
 )
+type router struct {
+	router *mux.Router
+	perm Permissions
+	server Server
+}
 
-func NewRouter(s Server) *mux.Router {
-	permissions := Const().Permissions
-	router := mux.NewRouter()
+func (r router) build() {
+	r.perm = Const().Permissions
+	r.userResource()
+	r.deviceResource()
+	r.permissionsResource()
+	r.authResource()
+}
 
-	router.HandleFunc("/device/register", adapters.Adapt(s.RegisterHandler(), adapters.ResponseTime(s.Logger))).
+func (r router) deviceResource() {
+	r.router.HandleFunc("/device/register", adapters.Adapt(r.server.RegisterHandler(), adapters.ResponseTime(r.server.Logger))).
 		Methods("POST")
+}
 
-	router.HandleFunc("/session/", s.LoginHandler()).
-		Methods("POST")
-
-	router.HandleFunc("/session/refresh", s.RefreshTokenHandler()).
-		Methods("POST")
-
-	router.HandleFunc(
-		"/permissions", adapters.Adapt(s.ListPermissions(),
-			adapters.MustHavePermission(permissions.Permissions.List))).
-		Methods("GET")
-
-	router.HandleFunc(
+func (r router) userResource() {
+	r.router.HandleFunc(
 		"/users",
-		adapters.Adapt(s.CreateUser(), adapters.MustHavePermission(permissions.User.Create))).
+		adapters.Adapt(r.server.CreateUser(), adapters.MustHavePermission(r.perm.User.Create))).
 		Methods("PUT")
 
-	router.HandleFunc(
-		"/users", adapters.Adapt(s.ListUsers(),
-			adapters.MustHavePermission(permissions.User.List))).
+	r.router.HandleFunc(
+		"/users", adapters.Adapt(r.server.ListUsers(),
+			adapters.MustHavePermission(r.perm.User.List))).
 		Methods("GET")
 
-	router.Handle("/users/available", s.UserAvailability()).
+	r.router.Handle("/users/available", r.server.UserAvailability()).
 		Methods("POST")
-	return router
+}
+
+func (r router) permissionsResource() {
+	r.router.HandleFunc(
+		"/permissions", adapters.Adapt(r.server.ListPermissions(),
+			adapters.MustHavePermission(r.perm.Permissions.List))).
+		Methods("GET")
+}
+
+func (r router) authResource() {
+	r.router.HandleFunc("/session/", r.server.LoginHandler()).
+		Methods("POST")
+
+	r.router.HandleFunc("/session/refresh", r.server.RefreshTokenHandler()).
+		Methods("POST")
+}
+
+func NewRouter(s Server) *mux.Router {
+	muxRouter := mux.NewRouter()
+	routerInstance := router{
+		server: s,
+		router: muxRouter,
+	}
+	routerInstance.build()
+	return routerInstance.router
 }
