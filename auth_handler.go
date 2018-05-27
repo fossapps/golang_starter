6cd2d3bfe191016b0e45230ad9a695903e71fcab
Nishchal <gautam.nishchal@gmail.com>
2018-05-27 18:38:04 +0700

    chore(todo): remove absolete todo [ci skip]

--- auth_handler.go
+++ auth_handler.go
@@ -52,9 +52,6 @@ func (s *Server) LoginHandler() http.HandlerFunc {
 		}
 		jwtToken, err := s.Jwt.CreateForUser(user)
 		if err != nil {
-			// since it's a separate thing, maybe application init test should take care of it
-			// then we don't need to check if it had an error?
-			// todo can't test as of now, let's see.
 			s.Logger.Error("jwt error", err)
 			s.ErrorResponse(w, r, http.StatusInternalServerError, "error generating token")
 			return
@@ -102,9 +99,6 @@ func (s *Server) RefreshTokenHandler() http.HandlerFunc {
 		}
 		token, err = s.Jwt.CreateForUser(user)
 		if err != nil {
-			// since it's a separate thing, maybe application init test should take care of it
-			// then we don't need to check if it had an error?
-			// todo can't test as of now, let's see. (same comment in above handler, good idea to extract)
 			s.Logger.Error("token generation error: ", err)
 			s.ErrorResponse(w, r, http.StatusInternalServerError, "error generating token")
 			return
